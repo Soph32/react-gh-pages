b454717f2e29704696af288d174c35094a5e46bd
Sophie <33546750+Pl4typusRex@users.noreply.github.com>
2020-07-01 20:48:14 +0100

    homepage background basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import background from "./assets/homepage.jpg";
 import './App.css';
+//import NavBar from './navbar.js'
 
 function Homepage() {
   return (
     <div className="App">
+      <img className="background" src={background} alt="homepage background" />
       <Title />
       <NavBar />
+      <MainText />
     </div>
   );
+}
+
+function MainText() {
+  return (
+    <div  className="main-text">
+      <h1>Junior Developer</h1>
+      <h1>Other Text</h1>
+      <h1>Other Text</h1>
+    </div>
+  )
 }
 
 function Title() {
