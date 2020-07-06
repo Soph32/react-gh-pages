cc8edc460d62a5770b2145f3934eba59f0580228
Sophie <33546750+Pl4typusRex@users.noreply.github.com>
2020-07-06 08:51:32 +0100

    added temp background and styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import background from "./assets/homepage.jpg";
 import './App.css';
-//import NavBar from './navbar.js'
+import Header from './header.js'
 
 function Homepage() {
   return (
     <div className="App">
       <img className="background" src={background} alt="homepage background" />
-      <Title />
-      <NavBar />
+      <Header />
       <MainText />
     </div>
   );
@@ -17,29 +16,11 @@ function Homepage() {
 function MainText() {
   return (
     <div  className="main-text">
-      <h1>Junior Developer</h1>
-      <h1>Other Text</h1>
-      <h1>Other Text</h1>
+      <h1 id="first-main-text">Junior Developer</h1>
+      <h1 id="second-main-text">Other Text</h1>
+      <h1 id="third-main-text">Other Text</h1>
     </div>
   )
-}
-
-function Title() {
-  return (
-    <div>
-      <h1>React App</h1>
-    </div>
-  );
-}
-
-function NavBar() {
-  return (
-    <div className="nav-bar">
-      <a href='#'>About</a>
-      <a href='#'>Blog</a>
-      <a href='#'>Projects</a>
-    </div>
-  );
 }
 
 export default Homepage;
